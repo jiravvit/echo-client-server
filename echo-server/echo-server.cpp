2a5983666f0fbb3c41ba5640d69f504f94f1e48d
jiravvit <jirabbit05@gmail.com>
2022-01-16 05:29:57 -0800

    Add a broadcast in echo-client and echo-server

--- echo-server/echo-server.cpp
+++ echo-server/echo-server.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,55 +11,140 @@
 #include "../mingw_net.h"
 #endif // WIN32
 #include <thread>
+#include <vector> // std::vector
+#include <algorithm> // iterator
 
 #ifdef WIN32
 void perror(const char* msg) { fprintf(stderr, "%s %ld\n", msg, GetLastError()); }
 #endif // WIN32
 
-using namespace std;
+std::vector<int> clients; // for broadcast
 
-void usage() 
-{
-	cout << "echo-server:\n";
-	cout << "syntax: echo-server <port> [-e[-b]]\n";
-	cout << "sample: echo-server 1234 -e -b\n";
+void usage() {
+	printf("syntax: echo-server <port> [-e[-b]]\n");
+	printf("  -e : echo\n");
+	printf("  -b : broadcast\n");
+	printf("sample: echo-server 1234 -e -b\n");
 }
 
-struct Param 
-{
+struct Param {
 	bool echo{false};
 	bool broadcast{false};
 	uint16_t port{0};
 
-	bool parse(int argc, char* argv[]) 
-	{
-		for (int i = 1; i < argc; i++) 
-		{
-			if (strcmp(argv[i], "-e") == 0) 
-			{
+	bool parse(int argc, char* argv[]) {
+		for (int i = 1; i < argc; i++) {
+			if (strcmp(argv[i], "-e") == 0) {
 				echo = true;
 				continue;
 			}
-			if (strcmp(argv[i], "-b") == 0) 
-			{
+			if (strcmp(argv[i], "-b") == 0) { // is it broadcast?
 				broadcast = true;
 				continue;
 			}
-			port = stoi(argv[i]);
+			port = atoi(argv[i]);
 		}
 		return port != 0;
 	}
 } param;
 
+void recvThread(int sd) {
+	printf("connected\n");
+	static const int BUFSIZE = 65536;
+	char buf[BUFSIZE];
+	while (true) {
+		ssize_t res = ::recv(sd, buf, BUFSIZE - 1, 0);
+		if (res == 0 || res == -1) {
+			fprintf(stderr, "recv return %ld", res);
+			perror(" ");
+			break;
+		}
+		buf[res] = '\0';
+		printf("%s", buf);
+		fflush(stdout);
+		if (param.echo) {
+			if (param.broadcast) {
+				for (int sd : clients) {	// for broadcast
+					res = ::send(sd, buf, res, 0);
+					if (res == 0 || res == -1) {
+						fprintf(stderr, "send return %ld", res);
+						perror(" ");
+						break;	
+					}
+				}
+			}
+			else {
+				res = ::send(sd, buf, res, 0);
+				if (res == 0 || res == -1) {
+					fprintf(stderr, "send return %ld", res);
+					perror(" ");
+					break;
+				}
+			}
+		}
+	}
+	printf("disconnected\n");
+	auto it = find(clients.begin(), clients.end(), sd);
+	clients.erase(it);	// erase iterator
+	clients.clear();	// clear vector
+	::close(sd);
+}
 
-int main(int argc, char* argv[]) 
-{
-	if (!param.parse(argc, argv)) 
-	{
+int main(int argc, char* argv[]) {
+	if (!param.parse(argc, argv)) {
 		usage();
 		return -1;
 	}
-	
-	return 0;
-}
 
+#ifdef WIN32
+	WSAData wsaData;
+	WSAStartup(0x0202, &wsaData);
+#endif // WIN32
+
+	int sd = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (sd == -1) {
+		perror("socket");
+		return -1;
+	}
+
+	int res;
+#ifdef __linux__
+	int optval = 1;
+	res = ::setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+	if (res == -1) {
+		perror("setsockopt");
+		return -1;
+	}
+#endif // __linux
+
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(param.port);
+
+	ssize_t res2 = ::bind(sd, (struct sockaddr *)&addr, sizeof(addr));
+	if (res2 == -1) {
+		perror("bind");
+		return -1;
+	}
+
+	res = listen(sd, 5);
+	if (res == -1) {
+		perror("listen");
+		return -1;
+	}
+
+	while (true) {
+		struct sockaddr_in cli_addr;
+		socklen_t len = sizeof(cli_addr);
+		int cli_sd = ::accept(sd, (struct sockaddr *)&cli_addr, &len);
+		if (cli_sd == -1) {
+			perror("accept");
+			break;
+		}
+		clients.push_back(cli_sd);	// for broadcast
+		std::thread* t = new std::thread(recvThread, cli_sd);
+		t->detach();
+	}
+	::close(sd);
+}
